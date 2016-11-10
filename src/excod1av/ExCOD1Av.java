07839307e8cc0cfb2066604a87f6b6004a884708
ggarciamartinez <ggarciamartinez@atanasoff20.danielcastelao.org>
2016-11-10 09:07:15 +0100

    A-segundo commit de la rama normal

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -16,6 +16,7 @@ public class ExCOD1Av {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Este es el primer comentario
         System.out.println("hola mundo");
     }
     
